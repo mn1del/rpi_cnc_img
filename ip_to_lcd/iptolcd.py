ac22bc2a2365566662e8344fa4702ad30f93618c
mn1del <davidleech@gmail.com>
2017-02-04 13:41:07 +0000

    testing

--- ip_to_lcd/iptolcd.py
+++ ip_to_lcd/iptolcd.py
@@ -44,18 +44,18 @@ data = p.communicate()  # Get data from 'p terminal'.
 # Split IP text block into three, and divide the two containing IPs into words.
 ip_lines = data[0].splitlines()
 split_line_a = ip_lines[1].split()
-split_line_b = ip_lines[2].split()
+# split_line_b = ip_lines[2].split()
 
 # con_type variables for the message text. ex) 'ethernet', 'wifi', etc.
 ip_type_a = connect_type(split_line_a)
-ip_type_b = connect_type(split_line_b)
+# ip_type_b = connect_type(split_line_b)
 
 """Because the text 'src' is always followed by an ip address,
 we can use the 'index' function to find 'src' and add one to
 get the index position of our ip.
 """
 ipaddr_a = split_line_a[split_line_a.index('src')+1]
-ipaddr_b = split_line_b[split_line_b.index('src')+1]
+# ipaddr_b = split_line_b[split_line_b.index('src')+1]
 
 # Creates a sentence for each ip address.
 # my_ip_a = 'Your %s ip is %s' % (ip_type_a, ipaddr_a)
