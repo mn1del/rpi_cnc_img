43267dfd3a718699a42152b141c45d1fdb16b376
mn1del <mn1del@users.noreply.github.com>
2017-02-08 14:56:22 +0000

    helpy.py - a few python helper functions
    Primary interested in helping the RPi setup scripts

---
+++ helpy.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# helphelp.py
+# utility helper functions
+
+import re
+import shutil
+import os
+
+
+# chginfile() - replaces strFrom with strTo in file strFilePath
+# takes simple string input for strFrom
+def chginfile(strFilePath, strFrom, strTo):
+    fin = open(strFilePath, "r")  # input file
+    fout = open("temp.txt", "w")  # temporary output file
+    patt = re.compile(strFrom)  # pattern
+    for line in fin:
+        newline = patt.sub(strTo, line)  # replace strFrom with strTo
+        print(newline)
+        fout.write(newline)
+    fin.close()
+    fin = None
+    fout.close()
+    fout = None
+    shutil.move("temp.txt", strFilePath)  # replace original with temp.txt
+    patt = None
+    os.remove("temp.txt")
+
+
+# chginfile() - replaces regexFrom with strTo in file strFilePath
+# for example chginfile_re(fp,"\d.\d","1.2")
+# regexFrom argument needs to be passed in quotes
+# see http://www.rexegg.com/regex-quickstart.html for regex cheatsheet
+def chginfile_re(strFilePath, regexFrom, strTo):
+    fin = open(strFilePath, "r")  # input file
+    fout = open("temp.txt", "w")  # temporary output file
+    patt = re.compile(regexFrom)  # pattern
+    for line in fin:  # loop through each line in fin
+        newline = patt.sub(strTo, line)  # replace strFrom with strTo
+        print(newline)  # print to console, not necessary
+        fout.write(newline)  # write to temporary file fout
+    fin.close()
+    fin = None
+    fout.close()
+    fout = None
+    shutil.move("temp.txt", strFilePath)  # replace original with temp.txt
+    patt = None
+    os.remove("temp.txt")
