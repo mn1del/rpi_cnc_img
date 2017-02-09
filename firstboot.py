a17ee827b4dc99a386fb1c60368c13de473c9628
mn1del <mn1del@users.noreply.github.com>
2017-02-09 16:18:26 +0000

    Update firstboot.py

--- firstboot.py
+++ firstboot.py
@@ -9,10 +9,35 @@ import os
 
 debugmode = True  # if True, then requires a user prompt for every action
 
+# INITIAL CONFIGS (BEFORE REBOOT)
+
 # Expand Filesystem
 yesno = raw_input("Expand Filesystem? (y/n) ")
 if yesno == "y" or debugmode == False:
     sp.call(["sudo", "raspi-config", "nonint", "do_expand_rootfs"])
+
+# Enable arduino uploading
+# requires reboot
+yesno = raw_input("Enable Arduino uploading? (y/n) ")
+if yesno == "y" or debugmode == False:
+    sp.call(["sudo", "usermod", "-a", "-G", "dialout", "pi"])
+
+# Enable PiScreenDrivers and configure calibration settings
+# calibration steps come from http://ozzmaker.com/forums/topic/piscreen-raspberripi2-touchscreen-calibration/
+f = open("/boot/config.txt","a")
+f.write("dtoverlay=piscreen,speed=16000000,rotate=90")
+f.close
+sp.call(["sed", "-i", "/^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axes (?i)Swap*$ /s/^/#/", "/etc/X11/xinit/xinitrc"])
+sp.call(["sed", "-i", "/^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axis (?i)Inversion*$ /s/^/#/", "/etc/X11/xinit/xinitrc"])
+sp.call(["sudo", "mv", "etc/pointercal.xinput", "etc/pointercal.xinput_copy"])
+
+# upgrade packages
+yesno = raw_input("Upgrade packages? (y/n) ")
+if yesno == "y" or debugmode == False:
+    sp.call(["sudo", "aptitude", "safe-upgrade"])
+
+# ******* REBOOT HERE, then requires follow up first boot script *************
+
 
 # install zip/unzip
 yesno = raw_input("Install zip/unzip? (y/n) ")
@@ -77,27 +102,19 @@ if yesno == "y" or debugmode == False:
     sp.call(["arduino", "--upload", grbldir + "/examples/GRBLtoArduino.ino", "--port", "/dev/ttyUSB*"])  # check port name
     sp.call(["cd", "/home/pi"])
 
-# Enable arduino uploading
-yesno = raw_input("Enable Arduino uploading? (y/n) ")
-if yesno == "y" or debugmode == False:
-    sp.call(["sudo", "usermod", "-a", "-G", "dialout", "pi"])
-
 # clone bCNC
 yesno = raw_input("Clone bCNC? (yes/no)")
 if yesno == "y" or debugmode == False:
     sp.call(["sudo", "git", "clone","https://github.com/vlachoudis/bCNC.git"])
 
-# install TeamViewer
+# Create bCNC Desktop Shortcut 
+yesno = raw_input("Create bCNC Desktop Shortcut? (y/n) ")
+if yesno == "y" or debugmode == False:
+    # sp.call(["mv", "/home/pi/rpi_cnc_img/bCNC.desktop", "/home/pi/Desktop/"])
+    sp.call(["ln", "-s", "/home/pi/bCNC/bCNC.desktop", "/home/pi/Desktop/bCNC"])
+
+# Download TeamViewer
 yesno = raw_input("Install TeamViewer? (y/n) ")
 if yesno == "y" or debugmode == False:
     sp.call(["wget", "http://download.teamviewer.com/download/linux/version_11x/teamviewer-host_armhf.deb"])
 
-# Create bCNC Desktop Shortcut 
-yesno = raw_input("Create bCNC Desktop Shortcut? (y/n) ")
-if yesno == "y" or debugmode == False:
-    sp.call(["mv", "/home/pi/rpi_cnc_img/bCNC.desktop", "/home/pi/Desktop/"])
-
-# Enable PiScreenDrivers
-f = open("/boot/config.txt","a")
-f.write("dtoverlay=piscreen,speed=16000000,rotate=90")
-f.close
