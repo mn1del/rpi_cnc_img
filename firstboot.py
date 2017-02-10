b99d4a63433bb94778948adf1bb3a2f416fc7f84
mn1del <davidleech@gmail.com>
2017-02-10 21:59:46 +0000

    correcting sed statement

--- firstboot.py
+++ firstboot.py
@@ -23,22 +23,15 @@ if yesno == "y" or debugmode == False:
 # calibration steps come from http://ozzmaker.com/forums/topic/piscreen-raspberripi2-touchscreen-calibration/
 yesno = raw_input("Enable Piscreen? (y/n) ")
 if yesno == "y" or debugmode == False:
-    #h.cmdcall("sudo sed -i """$ a\dtoverlay=piscreen,speed=16000000,rotate=90""" /boot/config.txt")
     sp.call(["sudo", "sed", "-i", "$ a\dtoverlay=piscreen,speed=16000000,rotate=90", "/boot/config.txt"])
-    # f = open("/boot/config.txt","a")
-    # f.write("dtoverlay=piscreen,speed=16000000,rotate=90")
-    # f.close
-    #h.cmdcall("sed -i /^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axes (?i)Swap*$ /s/^/#/ /etc/X11/xinit/xinitrc")
     sp.call(["sudo", "sed", "-i", "/^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axes (?i)Swap*$ /s/^/#/", "/etc/X11/xinit/xinitrc"])
-    # sp.call(shellcmd.split())
-    #h.cmdcall("sed -i /^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev Axis (?i)Inversion*$ /s/^/#/ /etc/X11/xinit/xinitrc")
     sp.call(["sudo", "sed", "-i", "/^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axis (?i)Inversion*$ /s/^/#/", "/etc/X11/xinit/xinitrc"])
-    # sp.call(shellcmd.split())
+
     if os.path.isfile("etc/pointercal.xinput"):
         h.cmdcall("sudo mv etc/pointercal.xinput etc/pointercal.xinput_copy")
-        # sp.call(shellcmd.split())
 
 # upgrade packages
+# perhaps one for second boot?
 yesno = raw_input("Upgrade packages? (y/n) ")
 if yesno == "y" or debugmode == False:
     h.cmdcall("sudo aptitude safe-upgrade -y")
@@ -46,6 +39,7 @@ if yesno == "y" or debugmode == False:
 #   sp.call(["sudo", "aptitude", "safe-upgrade"])
 
 # set call for secondboot.py
-f = open("/etc/rc.local","a")
-f.write("sleep 10;python /home/pi/rpi_cnc_img/secondboot.py")
-f.close
+sp.call(["sudo", "sed", "-i", "$ a\sleep 10;python /home/pi/rpi_cnc_img/secondboot.py", "/etc/rc.local"])
+#f = open("/etc/rc.local","a")
+#f.write("sleep 10;python /home/pi/rpi_cnc_img/secondboot.py")
+#f.close
