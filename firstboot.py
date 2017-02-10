93f784e05698c0e3e716c85edfda0fdc5e7fd5fe
mn1del <mn1del@users.noreply.github.com>
2017-02-10 15:59:05 +0000

    Update firstboot.py

--- firstboot.py
+++ firstboot.py
@@ -6,6 +6,7 @@
 
 import subprocess as sp
 import os
+import helpy as h
 
 debugmode = True  # if True, then requires a user prompt for every action
 
@@ -14,32 +15,36 @@ debugmode = True  # if True, then requires a user prompt for every action
 # Expand Filesystem
 yesno = raw_input("Expand Filesystem? (y/n) ")
 if yesno == "y" or debugmode == False:
-    sp.call(["sudo", "raspi-config", "nonint", "do_expand_rootfs"])
+    h.cmdcall("sudo raspi-config nonint do_expand_rootfs")
+#    sp.call(["sudo", "raspi-config", "nonint", "do_expand_rootfs"])
 
 # Enable arduino uploading
 # requires reboot
 yesno = raw_input("Enable Arduino uploading? (y/n) ")
 if yesno == "y" or debugmode == False:
-    sp.call(["sudo", "usermod", "-a", "-G", "dialout", "pi"])
+    h.cmdcall("sudo usermod -a -G dialout pi")
+#    sp.call(shellcmd.split())
 
 # Enable PiScreenDrivers and configure calibration settings
 # calibration steps come from http://ozzmaker.com/forums/topic/piscreen-raspberripi2-touchscreen-calibration/
 f = open("/boot/config.txt","a")
 f.write("dtoverlay=piscreen,speed=16000000,rotate=90")
 f.close
-sp.call(["sed", "-i", "/^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axes (?i)Swap*$ /s/^/#/", "/etc/X11/xinit/xinitrc"])
-sp.call(["sed", "-i", "/^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axis (?i)Inversion*$ /s/^/#/", "/etc/X11/xinit/xinitrc"])
-sp.call(["sudo", "mv", "etc/pointercal.xinput", "etc/pointercal.xinput_copy"])
+h.cmdcall("sed -i /^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axes (?i)Swap*$ /s/^/#/ /etc/X11/xinit/xinitrc")
+# sp.call(shellcmd.split())
+h.cmdcall("sed -i /^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev Axis (?i)Inversion*$ /s/^/#/ /etc/X11/xinit/xinitrc")
+# sp.call(shellcmd.split())
+h.cmdcall("sudo mv etc/pointercal.xinput etc/pointercal.xinput_copy")
+# sp.call(shellcmd.split())
 
 # upgrade packages
 yesno = raw_input("Upgrade packages? (y/n) ")
 if yesno == "y" or debugmode == False:
-    sp.call(["sudo", "aptitude", "safe-upgrade"])
+    h.cmdcall("sudo aptitude safe-upgrade -y")
+#    sp.call(shellcmd.split())
+#   sp.call(["sudo", "aptitude", "safe-upgrade"])
 
 # set call for secondboot.py
 f = open("/etc/rc.local","a")
 f.write("sleep 10;python /home/pi/rpi_cnc_img/secondboot.py")
 f.close
-
-# reboot
-sp.call(["sudo", "usermod", "-a", "-G", "dialout", "pi"])
