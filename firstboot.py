851eedd9733c10eeadec5c18445b0a46baf56427
mn1del <davidleech@gmail.com>
2017-02-10 21:50:05 +0000

    correcting sed statement

--- firstboot.py
+++ firstboot.py
@@ -21,19 +21,21 @@ if yesno == "y" or debugmode == False:
 
 # Enable PiScreenDrivers and configure calibration settings
 # calibration steps come from http://ozzmaker.com/forums/topic/piscreen-raspberripi2-touchscreen-calibration/
-#h.cmdcall("sudo sed -i """$ a\dtoverlay=piscreen,speed=16000000,rotate=90""" /boot/config.txt")
-sp.call(["sudo", "sed", "-i", "$ a\dtoverlay=piscreen,speed=16000000,rotate=90", "/boot/config.txt"])
-# f = open("/boot/config.txt","a")
-# f.write("dtoverlay=piscreen,speed=16000000,rotate=90")
-# f.close
-#h.cmdcall("sed -i /^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axes (?i)Swap*$ /s/^/#/ /etc/X11/xinit/xinitrc")
-sp.call(["sudo", "sed", "-i", "/^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axes (?i)Swap*$ /s/^/#/", "/etc/X11/xinit/xinitrc"])
-# sp.call(shellcmd.split())
-#h.cmdcall("sed -i /^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev Axis (?i)Inversion*$ /s/^/#/ /etc/X11/xinit/xinitrc")
-sp.call(["sudo", "sed", "-i", "/^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axis (?i)Inversion*$ /s/^/#/", "/etc/X11/xinit/xinitrc"])
-# sp.call(shellcmd.split())
-h.cmdcall("sudo mv etc/pointercal.xinput etc/pointercal.xinput_copy")
-# sp.call(shellcmd.split())
+yesno = raw_input("Enable Arduino uploading? (y/n) ")
+    if yesno == "y" or debugmode == False:
+    #h.cmdcall("sudo sed -i """$ a\dtoverlay=piscreen,speed=16000000,rotate=90""" /boot/config.txt")
+    sp.call(["sudo", "sed", "-i", "$ a\dtoverlay=piscreen,speed=16000000,rotate=90", "/boot/config.txt"])
+    # f = open("/boot/config.txt","a")
+    # f.write("dtoverlay=piscreen,speed=16000000,rotate=90")
+    # f.close
+    #h.cmdcall("sed -i /^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axes (?i)Swap*$ /s/^/#/ /etc/X11/xinit/xinitrc")
+    sp.call(["sudo", "sed", "-i", "/^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axes (?i)Swap*$ /s/^/#/", "/etc/X11/xinit/xinitrc"])
+    # sp.call(shellcmd.split())
+    #h.cmdcall("sed -i /^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev Axis (?i)Inversion*$ /s/^/#/ /etc/X11/xinit/xinitrc")
+    sp.call(["sudo", "sed", "-i", "/^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axis (?i)Inversion*$ /s/^/#/", "/etc/X11/xinit/xinitrc"])
+    # sp.call(shellcmd.split())
+    h.cmdcall("sudo mv etc/pointercal.xinput etc/pointercal.xinput_copy")
+    # sp.call(shellcmd.split())
 
 # upgrade packages
 yesno = raw_input("Upgrade packages? (y/n) ")
