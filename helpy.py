f237209bd8c3dc91738109b4ad87de13cd553e5a
mn1del <mn1del@users.noreply.github.com>
2017-02-10 16:05:51 +0000

    added cmdcall()
    cmdcall() is ahelper function for inputting commands in python as if in the command line

--- helpy.py
+++ helpy.py
@@ -5,11 +5,12 @@
 import re
 import shutil
 import os
-
+import subprocess
 
 # chginfile() - replaces strFrom with strTo in file strFilePath
 # takes simple string input for strFrom
 def chginfile(strFilePath, strFrom, strTo):
+    "replaces strFrom with strTo in file strFilePath"
     fin = open(strFilePath, "r")  # input file
     fout = open("temp.txt", "w")  # temporary output file
     patt = re.compile(strFrom)  # pattern
@@ -31,6 +32,7 @@ def chginfile(strFilePath, strFrom, strTo):
 # regexFrom argument needs to be passed in quotes
 # see http://www.rexegg.com/regex-quickstart.html for regex cheatsheet
 def chginfile_re(strFilePath, regexFrom, strTo):
+    "replaces regexFrom with strTo in file strFilePath"
     fin = open(strFilePath, "r")  # input file
     fout = open("temp.txt", "w")  # temporary output file
     patt = re.compile(regexFrom)  # pattern
@@ -45,3 +47,9 @@ def chginfile_re(strFilePath, regexFrom, strTo):
     shutil.move("temp.txt", strFilePath)  # replace original with temp.txt
     patt = None
     os.remove("temp.txt")
+
+# cmdcall() - takes a string and passes it into subprocess.call()
+# effectively mimics entering a command diarectly into the shell
+def cmdcall(commandString)
+    "calls command ""commandString"", as if entered in the CLI"
+    subprocess.call(commandString.split())
