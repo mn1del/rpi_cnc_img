b84defe0b74c4726dc38816429a0fe83af13169a
mn1del <mn1del@users.noreply.github.com>
2017-02-14 10:39:27 +0000

    cwd= arg in subprocess.call()
    to replace cd command in etc/rc.local

--- secondboot_arduino.py
+++ secondboot_arduino.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # deals with uploading grbl to arduino
 # placed in a separate script to secondboot.py because I couldn't figure out how to CD into 
-# th righ directory within python, so instead I'll do the "cd'ing" in rc.local, and call standalone script
+# the right directory within python, so instead I'll do the "cd'ing" in rc.local, and call standalone script***
+#*** EDIT: apparently passing cwd="directory" as an argument after the list in subprocess.call() is how to run the subprocess
+# from another directory - which would negate the need to "pre-cd" into the right directory in rc.local
 
 
 import subprocess as sp
@@ -14,13 +16,13 @@ if debugmode == False:
 else:
     yesno = raw_input("Upload GRBL to Arduino? (y/n) ")
 if yesno == "y" or debugmode == False:
-    #h.cmdcall("sudo make")  # test that the sketch compiles
-    sp.call(["sudo", "make", "upload"])  # upload to arduino
+    #sp.call(["sudo", "make"], cwd="")  # test that the sketch compiles
+    sp.call(["sudo", "make", "upload"], cwd="/usr/share/arduino/libraries/grbl/examples/GRBLtoArduino")  # upload to arduino
 
 # set call for everyboot.py
 # replaces previous call for secondboot.py
-sp.call(["sudo", "sed", "-i", "/cd \/home\/pi/,/^exit 0/{//!d}", "/etc/rc.local"])
-sp.call(["sudo", "sed", "-i", "/^exit 0/i \python /home/pi/rpi_cnc_img/everyboot.py", "/etc/rc.local"])
+#sp.call(["sudo", "sed", "-i", "/cd \/home\/pi/,/^exit 0/{//!d}", "/etc/rc.local"])
+sp.call(["sudo", "sed", "-i", "/^exit 0/i \python /home/pi/rpi_cnc_img/everyboot.py", "/etc/rc.local"]) #check backslashes
 sp.call(["sudo", "sed", "-i", "/^exit 0/i \sudo startx", "/etc/rc.local"])
 sp.call(["sudo", "sed", "-i", "/^exit 0/i \\/home\/pi\/bCNC\/bCNC"])
 sp.call(["sudo", "sed", "-i", "/^exit 0/i \chromium-browser --kiosk http://localhost:8080", "/etc/rc.local"])
