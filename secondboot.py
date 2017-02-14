7356ee15252fae1cf38238113e02815abd48cf97
mn1del <mn1del@users.noreply.github.com>
2017-02-14 10:17:54 +0000

    Update secondboot.py

--- secondboot.py
+++ secondboot.py
@@ -101,11 +101,16 @@ if yesno == "y" or debugmode == False:
     sp.call(["sudo", "touch", grbldir])
     sp.call(["sudo", "chmod", "777", grbldir])
     sketch = open(grbldir,"w")
-    sketch.write("ARDUINO_DIR = /usr/share/arduino\n")
-    sketch.write("BOARD_TAG = uno\n")
-    sketch.write("ARDUINO_PORT = /dev/ttyACM0\n")
-    sketch.write("ARDUINO_LIBS = grbl\n")
-    sketch.write("include /usr/share/arduino/Arduino.mk\n")
+    sketch.write("ARDUINO_DIR = /usr/share/arduino\n
+                    BOARD_TAG = uno\n
+                    ARDUINO_PORT = /dev/ttyACM0\n
+                    ARDUINO_LIBS = grbl\n
+                    include /usr/share/arduino/Arduino.mk\n")
+    #sketch.write("ARDUINO_DIR = /usr/share/arduino\n")
+    #sketch.write("BOARD_TAG = uno\n")
+    #sketch.write("ARDUINO_PORT = /dev/ttyACM0\n")
+    #sketch.write("ARDUINO_LIBS = grbl\n")
+    #sketch.write("include /usr/share/arduino/Arduino.mk\n")
     sketch.close()
 
 # clone bCNC
@@ -122,7 +127,7 @@ if debugmode == False:
 else:
     yesno = raw_input("Create bCNC Desktop Shortcut? (y/n) ")
 if yesno == "y" or debugmode == False:
-    sp.call(["ln", "-s", "/home/pi/bCNC/bCNC.desktop", "/home/pi/Desktop/bCNC"])
+    sp.call(["ln", "-s", "/home/pi/bCNC/bCNC", "/home/pi/Desktop/bCNC"])
 
 # Download TeamViewer
 if debugmode == False:
@@ -134,13 +139,17 @@ if yesno == "y" or debugmode == False:
 
 # Set up X Windows for the piscreen
 sp.call(["sudo", "DEBIAN_FRONTEND=noninteractive", "aptitude", "-y", "-q", "-o", "Dpkg::Options::=""--force-confdef""", "-o", "Dpkg::Options::=""--force-confold""",  "install", "x11-xserver-utils"])
+#turn off screenssaver with disable.sh
 sp.call(["sudo", "mv", "/home/pi/rpi_cnc_img/disableblank.sh", "/etc/X11/Xsession.d/"])
 sp.call(["sudo", "chmod", "+x", "/etc/X11/Xsession.d/disableblank.sh"])
-sp.call(["sudo", "sed", "-i", "$ a\/etc/X11/Xsession.d/disableblank.sh", "/etc/xdg/lxsession/LXDE-pi/autostart"])
+################################ backslashes needed?##########################################################
+sp.call(["sudo", "sed", "-i", "$ a\/etc/X11/Xsession.d/disableblank.sh", "/etc/xdg/lxsession/LXDE-pi/autostart"]) # backslashes needed???
+################################ backslashes needed?##########################################################
 
 # set login to GUI autologin
 #auto login
 sp.call(["sudo", "sed", "-i", "s/1:12345:respawn:\/sbin\/getty 115200 tty1/1:2345:respawn:\/bin\/login -f pi tty1 <\/dev\/tty1 >\/dev\/tty1 2>&1/", "/etc/rc.local"])
 #auto startx
 sp.call(["sudo", "sed", "-i", "/^exit 0/i \sudo startx", "/etc/rc.local"])
+#extra step for Jessie. From http://ozzmaker.com/piscreen-driver-install-instructions-2/
 sp.call(["sudo", "sed", "-i", "s/\/dev\/fb0/\/dev\/fb1/", "/usr/share/X11/xorg.conf.d/99-fbturbo.conf"])
