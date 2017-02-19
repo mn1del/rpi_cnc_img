b40cacf0c9cfde619739403693fe112d3e5322ac
mn1del <davidleech@gmail.com>
2017-02-19 18:51:10 +0000

    great grblsettings.py

---
+++ grblsettings.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# grblconfig.py
+# overwrites grbl settings in /home/pi/.bCNC
+# uses /home/pi/bCNC/bCNC.ini as the default template
+
+
+srcPath = "/home/pi/bCNC/bCNC.ini"
+tgtPath = "/home/pi/.bCNC"
+
