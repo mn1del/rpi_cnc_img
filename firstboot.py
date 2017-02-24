1816a460463e8a3e1e269c197be5df4a670b547a
mn1del <davidleech@gmail.com>
2017-02-24 21:03:40 +0000

    disable screen blanking from firstboot.py

--- firstboot.py
+++ firstboot.py
@@ -34,32 +34,35 @@ if debugmode == False:
 else:
     yesno = raw_input("Enable Piscreen? (y/n) ")
 if yesno == "y":
+    # enable PiScreen
     log = sp.call(["sudo", "sed", "-i", "$ a\dtoverlay=piscreen,speed=16000000,rotate=270", "/boot/config.txt"])
     bootlog.write('\nlog = sp.call(["sudo", "sed", "-i", "$ a\dtoverlay=piscreen,speed=16000000,rotate=270", "/boot/config.txt"])')
     bootlog.write(log)
     bootlog.write("\n")
-    # for boot to CLI:
+
+    #FOR BOOT TO COMMAND LINE
     log = sp.call(["cp", "/boot/cmdline.txt", "/home/pi/cmdline.txt"])   # backup cmdline.txt
     bootlog.write('\nlog = sp.call(["cp", "/boot/cmdline.txt", "/home/pi/cmdline.txt"])   # backup cmdline.txt')
     bootlog.write(log)
     bootlog.write("\n")
-
+    # configure correct orientation
     log = sp.call(["sudo", "sed", "-i", "1 s/$/ fbcon=map:10 fbcon=rotate:2 fbcon=font:ProFont6x11/", "/boot/cmdline.txt"])
     bootlog.write('\nlog = sp.call(["sudo", "sed", "-i", "1 s/$/ fbcon=map:10 fbcon=rotate:2 fbcon=font:ProFont6x11/", "/boot/cmdline.txt"])')
     bootlog.write(log)
     bootlog.write("\n")
 
-    #log = sp.call(
-    #log = sp.call(["sudo", "aptitude", "-y", "install", "x11-xserver-utils"])  # install X server utilities
-    #log = sp.call(["sudo", "sed", "-i", "/^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axes (?i)Swap*$ /s/^/#/", "/etc/X11/xinit/xinitrc"])
-    #log = sp.call(["sudo", "sed", "-i", "/^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axis (?i)Inversion*$ /s/^/#/", "/etc/X11/xinit/xinitrc"])
+    # output display to GPIO (PiScreen)
     log = sp.call(["sudo", "sed", "-i", "s/\/dev\/fb0/\/dev\/fb1/", "/usr/share/X11/xorg.conf.d/99-fbturbo.conf"])
     bootlog.write('\nlog = sp.call(["sudo", "sed", "-i", "s/\/dev\/fb0/\/dev\/fb1/", "/usr/share/X11/xorg.conf.d/99-fbturbo.conf"])')
     bootlog.write(log)
     bootlog.write("\n")
 
-    #if os.path.isfile("etc/pointercal.xinput"):
-        #h.cmdcall("sudo mv etc/pointercal.xinput etc/pointercal.xinput_copy")
+    # disable screen blanking (just once)
+    sp.call(["sudo", "xset", "s", "off"])
+    sp.call(["sudo", "xset", "-dpms"])
+    sp.call(["sudo", "xset", "s", "noblank"])
+    # disable screen balnking permanently (uncomment to effect this)
+    # sp.call(["sudo", "sed", "-i", "/\[SeatDefaults\]/ a\xserver-command=X -s 0 -dpms", "/etc/lightdm/lightdm.conf"])
 
 # set call for secondboot.py
 log = sp.call(["sudo", "sed", "-i", "/^exit 0/ i\sleep 10;python /home/pi/rpi_cnc_img/everyboot.py", "/etc/rc.local"])
