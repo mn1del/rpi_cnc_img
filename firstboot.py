d4395af8ce93159996abbdb62f19251e2b3ceb3c
mn1del <davidleech@gmail.com>
2017-02-24 20:47:06 +0000

    create firstboot logs

--- firstboot.py
+++ firstboot.py
@@ -9,6 +9,8 @@ import os
 #import helpy as h
 
 debugmode = False  # if True, then requires a user prompt for every action
+bootlog = open("/home/pi/bootlog.txt", "w")  # creat log file to track boot script progress
+
 
 # INITIAL CONFIGS (BEFORE REBOOT)
 
@@ -19,7 +21,10 @@ if debugmode == False:
 else:
     yesno = raw_input("Enable Arduino uploading? (y/n) ")
 if yesno == "y":
-    sp.call(["sudo", "usermod", "-a", "-G", "dialout", "pi"])
+    log = sp.call(["sudo", "usermod", "-a", "-G", "dialout", "pi"])
+    bootlog.write('\nlog = sp.call(["sudo", "usermod", "-a", "-G", "dialout", "pi"])')
+    bootlog.write(log)
+    bootlog.write("\n")
 
 # Enable PiScreenDrivers and configure for console use
 # http://ozzmaker.com/enable-console-on-piscreen/
@@ -29,22 +34,38 @@ if debugmode == False:
 else:
     yesno = raw_input("Enable Piscreen? (y/n) ")
 if yesno == "y":
-    sp.call(["sudo", "sed", "-i", "$ a\dtoverlay=piscreen,speed=16000000,rotate=270", "/boot/config.txt"])
+    log = sp.call(["sudo", "sed", "-i", "$ a\dtoverlay=piscreen,speed=16000000,rotate=270", "/boot/config.txt"])
+    bootlog.write('\nlog = sp.call(["sudo", "sed", "-i", "$ a\dtoverlay=piscreen,speed=16000000,rotate=270", "/boot/config.txt"])')
+    bootlog.write(log)
+    bootlog.write("\n")
     # for boot to CLI:
-    sp.call(["cp", "/boot/cmdline.txt", "/home/pi/cmdline.txt"])   # backup cmdline.txt
-    sp.call(["sudo", "sed", "-i", "1 s/$/ fbcon=map:10 fbcon=rotate:2 fbcon=font:ProFont6x11/", "/boot/cmdline.txt"])
+    log = sp.call(["cp", "/boot/cmdline.txt", "/home/pi/cmdline.txt"])   # backup cmdline.txt
+    bootlog.write('\nlog = sp.call(["cp", "/boot/cmdline.txt", "/home/pi/cmdline.txt"])   # backup cmdline.txt')
+    bootlog.write(log)
+    bootlog.write("\n")
 
-    #sp.call(
-    #sp.call(["sudo", "aptitude", "-y", "install", "x11-xserver-utils"])  # install X server utilities
-    #sp.call(["sudo", "sed", "-i", "/^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axes (?i)Swap*$ /s/^/#/", "/etc/X11/xinit/xinitrc"])
-    #sp.call(["sudo", "sed", "-i", "/^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axis (?i)Inversion*$ /s/^/#/", "/etc/X11/xinit/xinitrc"])
-    sp.call(["sudo", "sed", "-i", "s/\/dev\/fb0/\/dev\/fb1/", "/usr/share/X11/xorg.conf.d/99-fbturbo.conf"])
+    log = sp.call(["sudo", "sed", "-i", "1 s/$/ fbcon=map:10 fbcon=rotate:2 fbcon=font:ProFont6x11/", "/boot/cmdline.txt"])
+    bootlog.write('\nlog = sp.call(["sudo", "sed", "-i", "1 s/$/ fbcon=map:10 fbcon=rotate:2 fbcon=font:ProFont6x11/", "/boot/cmdline.txt"])')
+    bootlog.write(log)
+    bootlog.write("\n")
+
+    #log = sp.call(
+    #log = sp.call(["sudo", "aptitude", "-y", "install", "x11-xserver-utils"])  # install X server utilities
+    #log = sp.call(["sudo", "sed", "-i", "/^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axes (?i)Swap*$ /s/^/#/", "/etc/X11/xinit/xinitrc"])
+    #log = sp.call(["sudo", "sed", "-i", "/^DISPLAY=*xinput*(?i)Touchscreen*(?i)Evdev (?i)Axis (?i)Inversion*$ /s/^/#/", "/etc/X11/xinit/xinitrc"])
+    log = sp.call(["sudo", "sed", "-i", "s/\/dev\/fb0/\/dev\/fb1/", "/usr/share/X11/xorg.conf.d/99-fbturbo.conf"])
+    bootlog.write('\nlog = sp.call(["sudo", "sed", "-i", "s/\/dev\/fb0/\/dev\/fb1/", "/usr/share/X11/xorg.conf.d/99-fbturbo.conf"])')
+    bootlog.write(log)
+    bootlog.write("\n")
 
     #if os.path.isfile("etc/pointercal.xinput"):
         #h.cmdcall("sudo mv etc/pointercal.xinput etc/pointercal.xinput_copy")
 
 # set call for secondboot.py
-sp.call(["sudo", "sed", "-i", "/^exit 0/ i\sleep 10;python /home/pi/rpi_cnc_img/everyboot.py", "/etc/rc.local"])
+log = sp.call(["sudo", "sed", "-i", "/^exit 0/ i\sleep 10;python /home/pi/rpi_cnc_img/everyboot.py", "/etc/rc.local"])
+bootlog.write('\nlog = sp.call(["sudo", "sed", "-i", "/^exit 0/ i\sleep 10;python /home/pi/rpi_cnc_img/everyboot.py", "/etc/rc.local"])')
+bootlog.write(log)
+bootlog.write("\n")
 
 # reboot
 if debugmode == False:
@@ -52,5 +73,5 @@ if debugmode == False:
 else:
     yesno = raw_input("Reboot? (y/n) ")
 if yesno == "y" or debugmode == False:
-    sp.call(["sudo", "shutdown", "-r"])
+    log = sp.call(["sudo", "shutdown", "-r"])
 
